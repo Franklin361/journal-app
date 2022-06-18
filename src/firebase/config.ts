6b32ddf20c78a28cb419168367c2cd64835334ea
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-06-18 18:36:08 -0500

    [FIX] resolve import envirment vars

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,27 +2,15 @@ import { FirebaseOptions, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite'; 
 
-import { getEnvironments } from '../utils';
-
-const {
-  VITE_APIKEY,
-  VITE_AUTHDOMAIN,
-  VITE_DATABASEURL,
-  VITE_PROJECTID,
-  VITE_STORAGEBUCKET,
-  VITE_MESSAGINGSENDERID,
-  VITE_APPID,
-} = getEnvironments();
-
 
 const firebaseConfig:FirebaseOptions = {
-  apiKey: VITE_APIKEY,
-  authDomain: VITE_AUTHDOMAIN,
-  databaseURL: VITE_DATABASEURL,
-  projectId: VITE_PROJECTID,
-  storageBucket: VITE_STORAGEBUCKET,
-  messagingSenderId: VITE_MESSAGINGSENDERID,
-  appId: VITE_APPID,
+  apiKey: import.meta.env.VITE_APIKEY as string,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN as string,
+  databaseURL: import.meta.env.VITE_DATABASEURL as string,
+  projectId: import.meta.env.VITE_PROJECTID as string,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET as string,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID as string,
+  appId: import.meta.env.VITE_APPID as string,
 };
 
 // Initialize Firebase
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
-export * from './getEnvironments'
+
 export * from './loadNotes'
 export * from './validationForm'
