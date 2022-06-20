c0864a57191691723a2261bf7e0dc77c041733d8
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-06-19 20:38:27 -0500

    [STYLE] change size of the image in the modal

--- src/components/modal/ModalImage.tsx
+++ src/components/modal/ModalImage.tsx
@@ -1,18 +1,23 @@
-import { Button } from '..';
+import { Icon } from '../../assets';
 import { useAppDispatch } from '../../hooks';
 import { onCloseModal } from '../../redux';
 
-export const ModalImage = ({image}:{image:string}) => {
+export const ModalImage = ({ image }: { image: string }) => {
 
     const dispatch = useAppDispatch();
 
     const handleCloseModal = () => dispatch(onCloseModal());
 
     return (
-        <div className='fixed -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4 image-max z-30 modal-view'>
-            <img src={image} className='rounded border mx-auto w-full shadow-2xl shadow-neutral-focus' alt="aaa" />
-            <Button icon='close' label='close' onClick={handleCloseModal}
-                className='w-auto absolute -translate-y-2/4 top-2/4 -right-12 modal-view' secondary />
-        </div>
+        <>
+            <div className='fixed -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4 z-30 modal-view max-w-3xl w-4/6'>
+                <img src={image} className='rounded border mx-auto object-contain w-fill shadow-2xl shadow-neutral-focus' alt="aaa" />
+            </div>
+            <button
+                className='rounded-full z-50 bg-red-500 fixed w-16 h-16 flex justify-center items-center top-5 right-8 hover:bg-red-600 active:bg-red-400 btn p-0 border-none   '
+                onClick={handleCloseModal}>
+                <Icon name='close' className='text-white text-3xl' />
+            </button>
+        </>
     )
 }
